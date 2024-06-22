c0eb0ec4308dc323a7ac78a64246d73e9b8ab2dc
EliasEnns <elias0enns@gmail.com>
2024-06-21 22:39:38 -0300

    Testei o botão de logout

--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import './NavBar.css'
 import Login from './LoginButton'
+import { useAuth } from '../hooks/AuthProvider';
 
 function NavBar() {
     return (
@@ -9,11 +10,10 @@ function NavBar() {
                 <button className='NavButton'><Link to="/">Home</Link></button>
                 <button className='NavButton'><Link to="/Products">Products</Link></button>
                 <button className='NavButton'><Link to="/Dashboard">Dashboard</Link></button>
+                <button className='NavButton' onClick={useAuth().logOut}>Logout</button>
             </div>
 
             <div className='Login'><Login/></div>
-            
-
         </div>
     );
 }
